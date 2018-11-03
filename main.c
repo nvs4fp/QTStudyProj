6d3e162c41b49682d578d90d319645de2b1dabee
nvs4fp <lyrichard021@hotmail.com>
2018-11-03 17:05:02 +0800

    Change main.c add sub function

--- main.c
+++ main.c
@@ -1,7 +1,17 @@
 #include<stdio.h>
+int add(int a, int b);
+
 int main(int argc, char*argv)
 {
-	int temp=0;
-	return temp;
+
+	int a=100;
+	int b=20;
+	int result temp = add(a, b);
+	return 0;
+}
+
+int add(int a, int b)
+{
+	return a+b;
 }
 
